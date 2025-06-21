a7a1826c7b2ce542b5d689bdf1cfb2228ca32523
Sam Loeffler <samloeffler123@gmail.com>
2025-06-21 07:31:45 -0500

    changed order of directory and made unit tests

---
+++ AppBlocker.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using AppBlocker;
+using Xunit;
+using AppBlockerAddFilesToList;
+namespace AppBlocker.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        string time = "0600-1400";
+        var result = Files.CheckIfTimeFrameValid(time);
+        Assert.True(result);
+    }
+}
