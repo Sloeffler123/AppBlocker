bbc42deb907c8b9028955bc2f53390dc93b8bf7c
Sam Loeffler <samloeffler123@gmail.com>
2025-06-24 11:38:29 -0500

    deleted unused methods

--- AppBlocker/AppBlockerCore/Program.cs
+++ AppBlocker/AppBlockerCore/Program.cs
@@ -8,73 +8,31 @@ namespace AppBlockerAddFilesToList
         public static List<string> filePaths = new();
 
         public static string timeFrameToBlock;
-        // display how to use the app blocker
-        public static void HowToUse()
-        {
-            Console.WriteLine("To block apps you can either block a specific file or an entire folder. To do so the easiest way is to go to that file or folders location and copy the address location. When asked what file or folder to block paste in the path of the file you copied.");
-            Console.WriteLine();
-            Console.WriteLine("You can easily delete files and folders you have setup, however it comes with a price. Your valuable time. In order to delete a file or folder you must type down 200 words without messing up. Now I know that might sound a little ridicoulious but come on dont you wanna stop using these apps so much? Also you will have unlimited time to complete the type test, just make sure you dont enter with any mistakes or you will have to start all over.");
-        }
         // main loop
         public static void Main()
         {
-            HowToUse();
+            
             Console.WriteLine();
-            UserInterface();
+            
             RunApplication.DetectIfFilesAreRunning();
         }
-        // loop for the user to interact with and decide what they want to do
-        public static string PromptUserInterfaceInput()
-        {
-            Console.WriteLine("What would you like to do? (1) Add files and folders/(2) Display files and folders/(3) Delete files and folders/(4) Change time frame/(5) Exit");
-            Console.WriteLine();
-            string userInput = Console.ReadLine();
-            return userInput;
-        }
 
-        public static void UserInterface()
-        {
-            while (true)
-            {
-                string userInput = PromptUserInterfaceInput();
-
-                if (userInput == "1")
-                {
-                    AddFileOrDirectory();
-                }
-                else if (userInput == "2")
-                {
-                    DisplayFiles();
-                }
-                else if (userInput == "3")
-                {
-                    DeleteUserPath();
-                }
-                else if (userInput == "4")
-                {
-                    PromptUserForTimeFrame();
-                }
-                else if (userInput == "5")
-                {
-                    break;
-                }
-                else
-                {
-                    Console.WriteLine("Please enter a valid input");
-                }
-            }
-        }
         // check if file exists and add the path to a list
-        public static void AddFile(string path)
+        public static void CheckIfFileOrDirectory(string path)
         {
             if (File.Exists(path))
             {
-                if (path.EndsWith(".exe"))
-                {
-                    path = path.Replace(".exe", "");
-                }
-                Console.WriteLine($"{path} - Added");
-                filePaths.Add(path);
+                AddFile(path);
+            }
+        }
+        public static void AddFile(string path)
+        {
+            
+            if (path.EndsWith(".exe"))
+            {
+            path = path.Replace(".exe", "");
+            Console.WriteLine($"{path} - Added");
+            filePaths.Add(path);
             }
             else
             {
@@ -95,94 +53,7 @@ namespace AppBlockerAddFilesToList
                 Console.WriteLine($"{path} could not be found. Please double check spelling");
             }
         }
-
-        public static void WritePathsToBlock()
-        {
-            foreach (var word in filePaths)
-            {
-                File.WriteAllText("C:\\Users\\samlo\\OneDrive\\Desktop\\AppBlockerClean\\AppBlocker\\AppBlockerCore\\user_files_to_block.txt", word);
-            }
-        }
-
-        public static string PromptForFileOrDirectory()
-        {
-            Console.WriteLine("Would you like to add a (1) file (2) directory (3) exit?");
-            var userInput = Console.ReadLine();
-            return userInput;
-        }
-        public static string PromptUsersPath()
-        {
-            Console.WriteLine("Please input the path you would like to block");
-            var input = Console.ReadLine();
-            return input;
-        }
-        public static void AddFileOrDirectory()
-        {
-            while (true)
-            {
-                var userInput = PromptForFileOrDirectory();
-                Console.WriteLine();
-                if (userInput == "3")
-                {
-                    TimeFrameToBlock();
-                    WritePathsToBlock();
-
-                    break;
-                }
-                var input = PromptUsersPath();
-                
-                if (userInput == "1")
-                {
-                    AddFile(input);
-                }
-                else if (userInput == "2")
-                {
-                    AddDirectory(input);
-                }
-                else
-                {
-                    Console.WriteLine("Please enter a valid input");
-                }
-            }
-        }
-        // diplay files
-        public static void DisplayFiles()
-        {
-
-            foreach (var path in filePaths)
-            {
-                Console.WriteLine(path);
-            }
-        }
-        // delete file from list
-        public static string PromptUserForPathToDelete()
-        {
-            Console.WriteLine("Which file would you like to delete? Please input the path / (1) Exit");
-            DisplayFiles();
-            Console.WriteLine();
-            var userInput = Console.ReadLine();
-            return userInput;
-        }
-
-        public static void DeleteUserPath()
-        {
-            while (true)
-            {
-                var userInput = PromptUserForPathToDelete();
-                if (userInput == "1")
-                {
-                    break;
-                }
-                else if (!filePaths.Contains(userInput))
-                {
-                    Console.WriteLine("Please enter a valid path");
-                }
-                else if (UserTyping(userInput))
-                {
-                    Console.WriteLine($"{userInput} deleted");
-                }
-            }
-        }
+        
         // time frame to block
         public static bool TryGetValidTimeFrame(string input)
         {
@@ -198,35 +69,6 @@ namespace AppBlockerAddFilesToList
             return false;
         }
 
-        public static string PromptUserForTimeFrame()
-        {
-            Console.WriteLine("What time frame would you like to block these apps? EX: 0600-1400 / (1) Exit");
-            var userInput = Console.ReadLine();
-            return userInput;
-        }
-
-        public static void TimeFrameToBlock()
-        {
-            while (true)
-            {
-                var userInput = PromptUserForTimeFrame();
-                if (userInput == "1")
-                {
-                    Console.WriteLine("GoodBye");
-                    break;
-                }
-                if (TryGetValidTimeFrame(userInput))
-                {
-                    Console.WriteLine($"Apps will be blocked from {timeFrameToBlock}");
-                    File.WriteAllText("C:\\Users\\samlo\\OneDrive\\Desktop\\AppBlockerClean\\AppBlocker\\AppBlockerCore\\time_frame_to_block.txt", timeFrameToBlock);
-                    break;
-                }
-                else
-                {
-                    Console.WriteLine("Please input a valid time frame");
-                }
-            }
-        }
         // check if time frame given is valid (Helper method)
         public static bool CheckIfTimeFrameValid(string timeFrame)
         {
