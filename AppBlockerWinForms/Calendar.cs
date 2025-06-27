0794ed301b3f37ce21c135c23a996f9c7663f65b
Sam Loeffler <samloeffler123@gmail.com>
2025-06-27 11:34:34 -0500

    added method to compare times on the calendar layout

--- AppBlockerWinForms/Calendar.cs
+++ AppBlockerWinForms/Calendar.cs
@@ -1,5 +1,5 @@
 using AppBlockerWinForms;
-
+using AppBlockerRunApplication;
 namespace AppBlockerCalendarWinForms
 {
     public class Calendar
@@ -14,19 +14,30 @@ namespace AppBlockerCalendarWinForms
             Friday,
             Saturday
         }
-        public static void ShowCalendarElements(Control tableLayout, string days)
+        
+        public static void ShowCalendarElements(Control tableLayout, List<string> days)
         {
-            // split the days 
-            var splitDays = SplitDays(days);
+            
+            
 
             foreach (Control ctrl in tableLayout.Controls)
             {
-                MessageBox.Show(ctrl.Text);
-                if (ctrl.Text == "Friday")
+                foreach(var day in days)
                 {
-                    
+                    if (day == ctrl.Name)
+                    {
+                        // if this is true need to find the time frame in the other 
+                        // panel table layout
+
+                    }
                 }
             }
+        }
+        // find time the user input and compare it to the table layout panel
+        
+        public static string[] SplitTimes(string times)
+        {
+            return times.Split("-");
         }
         public static string[] SplitDays(string days)
         {
@@ -56,12 +67,27 @@ namespace AppBlockerCalendarWinForms
             }
             return daysList;
         }
-        public static void ShowCalendarTimesElements(Control tableLayout)
+        public static (int, int) ShowCalendarTimesElements(Control tableLayout, string times)
         {
+            // need to get the location of the text box and make a text box 
+            // based off of the most recent time and last time so the location should only have two numbers when this loop completes
+            var splitTimes = SplitTimes(times);
+            int locationOne = 0;
+            int locationTwo = 0;
             foreach (Control ctrl in tableLayout.Controls)
             {
-                MessageBox.Show(ctrl.Text);
+                if (ctrl.Text == splitTimes[0])
+                {
+                    locationOne = ctrl.Location.Y;
+                }
+                else if (ctrl.Text == splitTimes[1])
+                {
+                    locationTwo = ctrl.Location.Y;
+                    break;
+                }
             }
+            // returns the y coord only locationOne is the first time Y coord locationTwo is the second time Y coord
+            return (locationOne, locationTwo);
         }
     }
 }
